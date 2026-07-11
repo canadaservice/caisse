de9ab44a68361e2450aa640f5bc3468f43da85a2
canadaservice <serviceprive93@gmail.com>
2026-07-11 19:04:49 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('SECRET_PASSWORD', '0000');
 // Votre clé pawaPay incluse de manière sécurisée
 $pawaPayToken = "eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJ0dCI6IkFBVCIsInN1YiI6IjI4NzMiLCJtYXYiOiIxIiwiZXhwIjoyMDkzMjUyMTI3LCJpYXQiOjE3Nzc2MzI5MjcsInBtIjoiREFGLFBBRiIsImp0aSI6IjBhZDY0ZGZjLTA0NWMtNGE1NS04YjI3LThhZDdmNWQ1YjQyMSJ9.S5bEkSU7TzgfYGZbOIwXj55g-XcWqpzv2as9jbDmMl8sNgPz8GLJxWbGrJVrZmyaJ_bSch5MGb6FlVoUE3HtJg"; 
 
-// ADRESSE VALIDÉE DE L'API POUR LES RETRAITS
+// ADRESSE CORRIGÉE AVEC LA BARRE OBLIQUE FINALE POUR ÉVITER L'ERREUR 405
 $apiUrl = "https://pawapay.io"; 
 
 $message = "";
